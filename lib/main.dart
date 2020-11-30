824d913cea72e265a0a207993778cfc6b97ece38
SatYu26 <goyalsatyam8@gmail.com>
2020-12-01 03:09:00 +0530

    Release Build with playstore configs and minor bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "NewChat",
+      title: "Pinged",
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
